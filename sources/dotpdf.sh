1eb92ac143c197713841aee3cd2fc852661c5d99
Rico Mariani <ricomariani@outlook.com>
2023-09-05 06:21:21 -0700

    And the dotpndf file too

--- sources/dotpdf.sh
+++ sources/dotpdf.sh
@@ -5,6 +5,6 @@
 # LICENSE file in the root directory of this source tree.
 
 ./out/cql --dot < "$1" > "out/$1.dot" || exit 1
-dot "out/$1.dot" -Tpng -o "out/$1.png" || exit 1
-echo "Created out/$1.dot and made out/$1.png with it."
-open "out/$1.png"
+dot "out/$1.dot" -Tpdf -o "out/$1.pdf" || exit 1
+echo "Created out/$1.dot and made out/$1.pdf with it."
+open "out/$1.pdf"

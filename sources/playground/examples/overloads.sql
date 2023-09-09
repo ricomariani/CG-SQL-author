752e717c90cb37b65ee31cd4806fba7f154258db
Rico Mariani <ricomariani@outlook.com>
2023-09-09 00:12:26 -0700

    Added more examples in examples/overloads.sql
    This needs docs, those are coming soon.

--- sources/playground/examples/overloads.sql
+++ sources/playground/examples/overloads.sql
@@ -26,14 +26,41 @@ begin
    set result := case when x is null then "null" else x end;
 end;
 
+proc fmt_real_pounds(x real<pounds>, out result text)
+begin
+   set result := case when x is null then "null" else printf("%glbs", x) end;
+end;
+
+proc fmt_real_joules(x real<joules>, out result text)
+begin
+   set result := case when x is null then "null" else printf("%gJ", x) end;
+end;
+
 -- you could do something for blob and object too if you wanted
 
 CREATE PROC entrypoint ()
 BEGIN
-  let i := true;
-  let j := 5;
-  let k := 32L;
-  let l := 3.14;
-  let m := "foo";
-  call printf("bool:%s int:%s long:%s real:%s text:%s\n", i::fmt(), j::fmt(), k::fmt(), l::fmt(), m::fmt());
+  declare b bool;
+  set b := true;
+  let i := 5;
+  let l := 32L;
+  let r := 3.14;
+  let t := "foo";
+  -- this is only needed if null values are a possibility
+  -- otherwise the format string would work by itself
+  -- ::fmt converts the data to a string even if it's null
+  -- the normal runtime can't do that
+  call printf("bool:%s int:%s long:%s real:%s text:%s\n", b::fmt(), i::fmt(), l::fmt(), r::fmt(), t::fmt());
+
+  -- fmt_bool handles null too
+  set b := null;
+  call printf("bool:%s\n", b::fmt());
+
+  -- using type kind to further specify which formatter
+   declare energy real<joules>;
+   set energy := 100.5;
+   declare weight real<pounds>;
+   set weight := 203;
+
+  call printf("arg1: %s arg2: %s\n", energy:::fmt(), weight:::fmt());
 END;

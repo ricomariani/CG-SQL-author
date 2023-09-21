313a1630bd2640a2850014e8173f2f1aab2144a1
Rico Mariani <ricomariani@outlook.com>
2023-09-21 14:37:27 -0700

    Added 'create' to the signature for create_timer When making a new reference type your function needs create so that CQL knows that the caller owns the pointer, otherwise it's borrowed.

--- sources/playground/utils/timer.sql
+++ sources/playground/utils/timer.sql
@@ -1,6 +1,6 @@
 @echo c, "#include <utils/timer.c>\n"; 
 
-declare function create_timer() object<timer> not null;
+declare function create_timer() create object<timer> not null;
 declare function start_object_timer(timer object<timer> not null) object<timer> not null;
 declare function stop_object_timer(timer object<timer> not null) object<timer> not null;
 declare function print_object_timer(timer object<timer> not null) object<timer> not null;

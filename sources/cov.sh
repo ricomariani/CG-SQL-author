c3781c4ec3daaa2f6cc3772a7628a3c72721acc5
Rico Mariani <ricomariani@outlook.com>
2023-10-05 13:45:45 -0700

    Removing common/cov_common.sh
    The common/ .sh files were there because the Meta build needed to
    be different than the regular OSS build.  Supporting the Meta build
    is now a non-goal so I'm pulling these back into the normal place.
    
    Basically meta had the same cov_common.sh as OSS but a unique cov.sh.
    
    This is just weird now.

--- sources/cov.sh
+++ sources/cov.sh
@@ -6,11 +6,82 @@
 
 COV_EXTRA_ARGS=""
 
-# shellcheck disable=SC1091
-source common/cov_common.sh || exit 1
+COV_ARGS="${COV_COVERAGE_ARGS}"
 
 OUT_DIR="out"
+
 GCOVR=gcovr
+
+coverage() {
+  rm -f *.gcov
+
+  if ! ./test.sh --coverage $@
+  then
+    echo "you can't run coverage until the tests all pass"
+    return 1
+  fi
+
+  # super spammy, we only care about the errors
+  if ! gcov out/*.gcda >/dev/null
+  then
+    echo "error running gcov"
+    return 1
+  fi
+
+  echo generating ${OUT_DIR}/report.html
+  if ! "${GCOVR}" \
+            -g -k \
+            --html \
+            --html-details \
+            -o ${OUT_DIR}/report.html \
+            ${COV_EXTRA_ARGS} \
+            -e test \
+            -e tester \
+            -e linetester \
+            -e json_test \
+            -e out \
+            -e cql.c \
+            -e cql.y.c \
+            -e cqltest.c \
+            -e result_set_extension.c \
+            -e run_test_client.c \
+            -e query_plan_test.c \
+            -e run_test.c \
+            -e generated_upgrade \
+            -e upgrade
+  then
+    echo "error generating html"
+    return 1
+  fi
+
+  echo generating ${OUT_DIR}/report.txt
+  if ! "${GCOVR}" \
+              -g -k \
+              -o ${OUT_DIR}/report.txt \
+              ${COV_EXTRA_ARGS} \
+              -e test \
+              -e tester \
+              -e linetester \
+              -e json_test \
+              -e out \
+              -e cql.c \
+              -e cql.y.c \
+              -e cqltest.c \
+              -e result_set_extension.c \
+              -e run_test_client.c \
+              -e query_plan_test.c \
+              -e run_test.c \
+              -e generated_upgrade \
+              -e upgrade
+  then
+    echo "error generating plain text"
+    return 1
+  fi
+
+  rm -f *.gcov
+
+  return 0
+}
 
 if ! coverage $@
 then
@@ -20,3 +91,4 @@ fi
 
 cat "${OUT_DIR}/report.txt"
 exit 0
+

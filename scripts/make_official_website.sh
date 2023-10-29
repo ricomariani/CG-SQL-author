14e42896cb47b2b9046374d40a445e8a78566ca6
Joris Garonian <joris.garonian@gmail.com>
2023-10-29 18:53:58 +1100

    CICD — Compile new website with Hugo using ./make_official_website.sh

--- scripts/make_official_website.sh
+++ scripts/make_official_website.sh
@@ -2,9 +2,13 @@
 
 set -o errexit -o nounset -o pipefail
 
+# Usage in dev: ./make_official_website.sh server
+
 readonly SCRIPT_DIR_RELATIVE=$(dirname "$0")
 
-hugo server \
+git submodule update --init --recursive
+
+hugo \
   --source $SCRIPT_DIR_RELATIVE/official_website \
   --destination ../out/official_website \
   $@

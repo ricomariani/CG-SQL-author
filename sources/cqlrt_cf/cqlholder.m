e8407c5edab715c2d143b5e66c90c7ba9094f874
Rico Mariani <ricomariani@outlook.com>
2024-02-19 06:40:53 -0800

    more holder comments

--- sources/cqlrt_cf/cqlholder.m
+++ sources/cqlrt_cf/cqlholder.m
@@ -19,6 +19,9 @@
 
 @implementation CQLHolder
 
+// The CQLHolder is a simple wrapper around a pointer and a type.  The type
+// tells us how to clean up the pointer when the holder is deallocated.
+// The holder is a CF object so it can be used with ARC.
 - (instancetype)initWithBytes:(void *)bytesIn andType:(int)typeIn
 {
   self = [super init];
@@ -27,6 +30,7 @@
   return self;
 }
 
+// The point of all this is use the dynamic teardown function to clean up
 - (void)dealloc
 {
   if (self.bytes)
@@ -40,11 +44,15 @@
 #define CF_HELD_TYPE_RESULT_SET 1
 #define CF_HELD_TYPE_GENERIC 2
 
+// The dynamic teardown function is the key to cleaning up the bytes
+// There is only one known type -- RESULT SET -- all other types
+// get the generic treatment... which is to call the finalizer if there is one.
 - (void)dynamicTeardown
 {
   switch (self.type) {
   case CF_HELD_TYPE_RESULT_SET:
     {
+      // the result set knows how to clean itself up
       cql_result_set_ref ref = (__bridge cql_result_set_ref)self;
       cql_result_set *result_set = (cql_result_set *)self.bytes;
       result_set->meta.teardown(ref);
@@ -57,6 +65,11 @@
       if (self.generic_finalizer) {
         self.generic_finalizer(self.bytes);
       }
+
+      // we do not call free() or anything like that
+      // the finalizer is responsible for freeing the bytes
+      // since we don't even know how they were allocated
+      // or if they even need to be freed at all.
       self.bytes = NULL;
       self.type = 0;
       self.generic_finalizer = NULL;
@@ -64,6 +77,7 @@
     }
   }
 
+  // this path is only for the result set
   free(self.bytes);
   self.bytes = NULL;
   self.type = 0;
@@ -96,6 +110,10 @@ cql_result_set *_Nonnull cql_get_result_set_from_ref(cql_result_set_ref _Nonnull
   return result_set;
 }
 
+// This creates holder for the indicated blob of bytes
+// the provided finalizer to free the bytes when the holder is deallocated.
+// This is the private C interface to do this so basically it just calls
+// right back into the object C world to do the job.
 cql_object_ref _Nonnull _cql_generic_object_create(void *_Nonnull data, void (*_Nonnull finalizer)(void *_Nonnull))
 {
   CQLHolder *holder = [[CQLHolder alloc] initWithBytes:(void *)data andType:CF_HELD_TYPE_GENERIC];
@@ -103,6 +121,8 @@ cql_object_ref _Nonnull _cql_generic_object_create(void *_Nonnull data, void (*_
   return (__bridge_retained cql_object_ref)holder;
 }
 
+// This gives us the stored data back out of the holder, the caller is
+// expected to know what to do with it.
 void *_Nonnull _cql_generic_object_get_data(cql_object_ref _Nonnull ref)
 {
   CQLHolder *holder = (__bridge CQLHolder *)ref;

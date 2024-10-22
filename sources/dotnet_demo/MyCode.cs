a5d2446ab1d6eb7c77b5aa083285c6257a007a16
Rico Mariani <ricomariani@outlook.com>
2024-10-21 22:48:51 -0700

    comments for blob conversion

--- sources/dotnet_demo/MyCode.cs
+++ sources/dotnet_demo/MyCode.cs
@@ -110,19 +110,21 @@ public class MyCode {
     Expect(count == 5, "count == 5");
 
     for (int i = 0; i < count; i++) {
+      // extracting the blob and converting it to a string because we know it is one
       byte[] bytes = data.get_bytes(i);
       String? s = System.Text.Encoding.UTF8.GetString(bytes);
+
       Console.WriteLine(
-         "Row {0}: name:{1} blob:{2} age:{3}({4}) thing:{5:F6} key1:{6} key2:{7}({8})",
-         i, // 0
-         data.get_name(i), // 1
-         s, // 2
-         data.get_age(i), // 3
-         data.get_age_IsEncoded() ? "encoded" : "clear", // 4
-         data.get_thing(i), // 5
-         data.get_key1(i), // 6
-         data.get_key2(i), // 7
-         data.get_key2_IsEncoded() ? "encoded" : "clear" // 8
+        "Row {0}: name:{1} blob:{2} age:{3}({4}) thing:{5:F6} key1:{6} key2:{7}({8})",
+        i, // 0
+        data.get_name(i), // 1
+        s, // 2
+        data.get_age(i), // 3
+        data.get_age_IsEncoded() ? "encoded" : "clear", // 4
+        data.get_thing(i), // 5
+        data.get_key1(i), // 6
+        data.get_key2(i), // 7
+        data.get_key2_IsEncoded() ? "encoded" : "clear" // 8
      );
 
       // this could be done automatically in the helper, it just isn't yet
@@ -142,6 +144,6 @@ public class MyCode {
   }
 
   public static void Expect(bool b, String str) {
-     if (!b) throw new Exception(str);
+    if (!b) throw new Exception(str);
   }
 }

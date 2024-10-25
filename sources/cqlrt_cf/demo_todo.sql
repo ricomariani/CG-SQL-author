05941acf4122beeaddfdb090512bea9c9deadb48
Rico Mariani <ricomariani@outlook.com>
2024-10-24 20:42:12 -0700

    formatting cleanups for demo_todo

--- sources/cqlrt_cf/demo_todo.sql
+++ sources/cqlrt_cf/demo_todo.sql
@@ -8,36 +8,39 @@
 -- This is a simple schema for keep track of tasks and whether they are done
 
 -- this serves to both declare the table and create the schema
-create proc todo_create_tables()
+proc todo_create_tables ()
 begin
-
   create table if not exists tasks(
-    description text not null,
-    done bool default false not null
+    description text!,
+    done bool! default false
   );
-
 end;
 
 -- adds a new not-done task
-create proc todo_add(task TEXT NOT null)
+proc todo_add (task text!)
 begin
-  insert into tasks values(task, false);
+  insert into tasks
+    values(task, false);
 end;
 
 -- gets the tasks in inserted order
-create proc todo_tasks()
+proc todo_tasks ()
 begin
-  select rowid, description, done from tasks order by rowid;
+  select rowid, description, done
+    from tasks
+    order by rowid;
 end;
 
 -- updates a given task by rowid
-create proc todo_setdone_(rowid_ integer not null, done_ bool not null)
+proc todo_setdone_ (rowid_ int!, done_ bool!)
 begin
-  update tasks set done = done_ where rowid == rowid_;
+  update tasks
+    set done = done_
+    where rowid = rowid_;
 end;
 
 -- deletes a given task by rowid
-create proc todo_delete(rowid_ integer not null)
+proc todo_delete (rowid_ int!)
 begin
-  delete from tasks where rowid == rowid_;
+  delete from tasks where rowid = rowid_;
 end;

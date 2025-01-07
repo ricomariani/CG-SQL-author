9c1f78a1f553066c98181ff2b74083c2a3e7920f
Rico Mariani <ricomariani@outlook.com>
2025-01-07 11:59:04 -0800

    make.sh temporary changes accidentally landed, restoring

--- sources/cqlrt_cf/make.sh
+++ sources/cqlrt_cf/make.sh
@@ -7,17 +7,20 @@
 # exit when any command fails
 set -e
 
+echo "building cql"
+(cd .. ; make)
+
 echo "building C code"
-./cql --in demo_todo.sql --cg demo_todo.h demo_todo.c --cqlrt cqlrt_cf.h
+../out/cql --in demo_todo.sql --cg demo_todo.h demo_todo.c --cqlrt cqlrt_cf.h
 
 echo "building JSON"
-./cql --in demo_todo.sql --cg demo_todo.json --rt json_schema
+../out/cql --in demo_todo.sql --cg demo_todo.json --rt json_schema
 
 echo "building OBJC code"
 ./cqlobjc.py demo_todo.json --objc_c_include_path demo_todo.h >demo_objc.h
 
 echo "building executable"
-cc -o demo -g -I../../beta -I. demo_todo.c demo_main.m cqlrt_cf.c cqlholder.m -lsqlite3 -framework Foundation -fobjc-arc
+cc -o demo -g -I.. -I. demo_todo.c demo_main.m cqlrt_cf.c cqlholder.m -lsqlite3 -framework Foundation -fobjc-arc
 
 echo "running demo"
 ./demo

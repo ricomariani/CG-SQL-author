4b165665397e4a5a49b76a9963a0a049bda1f496
Rico Mariani <ricomariani@outlook.com>
2025-03-23 23:30:33 -0700

    Added some comments in cqltest.c indicating purpose

--- sources/test/cqltest.c
+++ sources/test/cqltest.c
@@ -10,6 +10,13 @@
 extern cql_code cql_startup(sqlite3 *db);
 extern cql_code run_client(sqlite3 *db);
 
+// This is some basic "main" code to run the two run test sources
+// there are some manual tests from the C world which are in
+// run_client.  These basically exercise the CQL ABI from the C
+// perspective. There was once a time when a lot of things had
+// to be done this way because CQL could not consume itself.
+// The primary tests are the cql self-authoered ones from run_test.sql.
+// those start at cql_startup.
 int main(int argc, char **argv) {
   // Setup database
   sqlite3 *db = NULL;
@@ -23,13 +30,12 @@ int main(int argc, char **argv) {
   sqlite3_close(db);
   if (rc) exit(rc);
 
-  // now try again using the stored procs
+  // now try again using the stored procs and a clean db
   rc = sqlite3_open(":memory:", &db);
-
   if (rc == SQLITE_OK) {
     rc = cql_startup(db);
   }
-  sqlite3_close(db);
 
+  sqlite3_close(db);
   exit(rc);
 }

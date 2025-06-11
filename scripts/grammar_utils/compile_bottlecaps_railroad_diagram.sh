e885288ae317eb3b19b3143a87aba0fa2e032869
Rico Mariani <ricomariani@outlook.com>
2025-06-11 14:09:34 -0700

    update to rr2.5

--- scripts/grammar_utils/compile_bottlecaps_railroad_diagram.sh
+++ scripts/grammar_utils/compile_bottlecaps_railroad_diagram.sh
@@ -4,7 +4,7 @@ set -o errexit -o nounset -o pipefail
 
 readonly SCRIPT_DIR_RELATIVE=$(dirname "$0")
 
-RAILROAD_PATH=${RAILROAD_PATH:-~/rr-2.2-java11}
+RAILROAD_PATH=${RAILROAD_PATH:-~/rr-2.5-java11}
 
 guard_against_missing_rr() {
   if [ ! -f $RAILROAD_PATH/rr.war ]; then
@@ -21,9 +21,9 @@ Follow the instructions provided after the installation. It should looke like th
 $ sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
 (2) Install railroad diagram generator
-$ wget https://rr.red-dove.com/download/rr-2.2-SNAPSHOT-java11.zip
-$ unzip rr-2.2-java11.zip -d ./rr-2.2-java11
-$ mv ./rr-2.2-java11 ~/rr-2.2-java11
+$ wget https://rr.red-dove.com/download/rr-2.5-SNAPSHOT-java11.zip
+$ unzip rr-2.5-java11.zip -d ./rr-2.5-java11
+$ mv ./rr-2.5-java11 ~/rr-2.5-java11
 
 Note:
 The above is not the official site for this software - that would be https://www.bottlecaps.de/rr/ui. 
